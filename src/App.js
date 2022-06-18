8034198701abac1425d3de7e2c66352c7d2b271f
savitriazahranf <muhammad1800016137@webmail.uad.ac.id>
2022-06-18 22:55:56 +0700

    todo-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import HomePage from "./pages/Home";
+import "./App.css";
+import AddPage from "./pages/Add";
+import styled from "styled-components";
+import EditPage from "./pages/Edit";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+const Container = styled.div`
+	text-align: center;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Container>
+			<Routes>
+				<Route path="/add" element={<AddPage />} />
+				{/* Menerima paramameter Id */}
+				<Route path="/edit/:id" element={<EditPage />} />
+				<Route path="/" element={<HomePage />} />
+			</Routes>
+		</Container>
+	);
 }
 
 export default App;
